627950cdc7a5ca9388c2e9cfb5dc2890bd6e992b
hoaht <hoaht@aisantech.vn>
2015-10-29 09:15:33 +0700

    create pagerAdapter, SongListFragment

---
+++ app/src/main/java/com/example/asiantech/musicdemo/fragment/AlbumsFragment.java
@@ -0,0 +1,14 @@
+package com.example.asiantech.musicdemo.fragment;
+
+import android.support.v4.app.Fragment;
+
+import com.example.asiantech.musicdemo.R;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * Created by hoaht on 28/10/2015.
+ */
+@EFragment(R.layout.albums_fragment)
+public class AlbumsFragment extends Fragment {
+}

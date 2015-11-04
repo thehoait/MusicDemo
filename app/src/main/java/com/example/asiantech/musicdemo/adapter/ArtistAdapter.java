e92cd7763eaed59e3f501312fee28ff1f1baa681
hoaht <hoaht@aisantech.vn>
2015-11-04 10:46:28 +0700

    edit adapter list artist

--- app/src/main/java/com/example/asiantech/musicdemo/adapter/ArtistAdapter.java
+++ app/src/main/java/com/example/asiantech/musicdemo/adapter/ArtistAdapter.java
@@ -1,49 +1,59 @@
 package com.example.asiantech.musicdemo.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.example.asiantech.musicdemo.OnItemListener;
 import com.example.asiantech.musicdemo.R;
 import com.example.asiantech.musicdemo.model.Artist;
 
 import java.util.ArrayList;
 
-public class ArtistAdapter extends BaseAdapter {
+public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistViewHolder> {
     private Context mContext;
     private ArrayList<Artist> mListArtist;
+    private OnItemListener mListener;
 
-    public ArtistAdapter(Context context, ArrayList<Artist> listArtist) {
+    public ArtistAdapter(Context context, ArrayList<Artist> listArtist, OnItemListener listener) {
         this.mContext = context;
         this.mListArtist = listArtist;
+        this.mListener = listener;
     }
 
     @Override
-    public int getCount() {
+    public ArtistViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_list_artist, parent, false);
+        return new ArtistViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ArtistViewHolder holder, int position) {
+        holder.mTvNameArtist.setText(mListArtist.get(position).getArtist());
+    }
+
+    @Override
+    public int getItemCount() {
         return mListArtist.size();
     }
 
-    @Override
-    public Object getItem(int position) {
-        return mListArtist.get(position);
-    }
+    class ArtistViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTvNameArtist;
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_list_album, parent,
-                    false);
+        public ArtistViewHolder(View itemView) {
+            super(itemView);
+            mTvNameArtist = (TextView) itemView.findViewById(R.id.tvNameArtist);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mListener != null) {
+                        mListener.onItemClick(getLayoutPosition());
+                    }
+                }
+            });
         }
-        TextView tvNameAlbum = (TextView) convertView.findViewById(R.id.tvNameAlbum);
-        tvNameAlbum.setText(mListArtist.get(position).getArtist());
-        return convertView;
     }
 }
--- app/src/main/java/com/example/asiantech/musicdemo/fragment/ArtistFragment.java
+++ app/src/main/java/com/example/asiantech/musicdemo/fragment/ArtistFragment.java
@@ -3,12 +3,14 @@ package com.example.asiantech.musicdemo.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.GridView;
 
 import com.example.asiantech.musicdemo.MainActivity_;
+import com.example.asiantech.musicdemo.OnItemListener;
 import com.example.asiantech.musicdemo.R;
 import com.example.asiantech.musicdemo.adapter.ArtistAdapter;
 import com.example.asiantech.musicdemo.model.Artist;
@@ -21,10 +23,11 @@ import org.androidannotations.annotations.ViewById;
 import java.util.ArrayList;
 
 @EFragment(R.layout.artist_fragment)
-public class ArtistFragment extends Fragment {
+public class ArtistFragment extends Fragment implements OnItemListener{
     private ArrayList<Song> mListSong;
-    @ViewById(R.id.gvArtist)
-    GridView mGvArtists;
+    private ArrayList<Artist> mListArtist;
+    @ViewById(R.id.recycleViewListArtist)
+    RecyclerView mRecyclerListArtist;
 
     @AfterViews
     void afterView() {
@@ -32,35 +35,35 @@ public class ArtistFragment extends Fragment {
         if (getActivity() instanceof MainActivity_) {
             mListSong = ((MainActivity_) getActivity()).getMListSong();
         }
-        final ArrayList<Artist> listArtist = new ArrayList<>();
+        mListArtist = new ArrayList<>();
         for (int i = 0; i < mListSong.size(); i++) {
             boolean exists = false;
-            for (int j = 0; j < listArtist.size(); j++) {
-                if (mListSong.get(i).getArtistId() == listArtist.get(j).getArtistId()) {
+            for (int j = 0; j < mListArtist.size(); j++) {
+                if (mListSong.get(i).getArtistId() == mListArtist.get(j).getArtistId()) {
                     exists = true;
                     break;
                 }
             }
             if (!exists) {
-                listArtist.add(new Artist(mListSong.get(i).getArtistId(),
+                mListArtist.add(new Artist(mListSong.get(i).getArtistId(),
                         mListSong.get(i).getArtist()));
             }
         }
-        ArtistAdapter adapter = new ArtistAdapter(getContext(), listArtist);
-        mGvArtists.setAdapter(adapter);
-        mGvArtists.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ArtistDetailFragment_ fragment = new ArtistDetailFragment_();
-                Bundle bundle = new Bundle();
-                bundle.putLong("artistId", listArtist.get(position).getArtistId());
-                fragment.setArguments(bundle);
-                FragmentTransaction transaction = getActivity().getSupportFragmentManager()
-                        .beginTransaction();
-                transaction.add(R.id.rlContainer, fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-            }
-        });
+        ArtistAdapter adapter = new ArtistAdapter(getContext(), mListArtist, this);
+        mRecyclerListArtist.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerListArtist.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        ArtistDetailFragment_ fragment = new ArtistDetailFragment_();
+        Bundle bundle = new Bundle();
+        bundle.putLong("artistId", mListArtist.get(position).getArtistId());
+        fragment.setArguments(bundle);
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager()
+                .beginTransaction();
+        transaction.add(R.id.rlContainer, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
